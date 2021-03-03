99e846346f18c4e722919a2aefc955308a75c808
he9mei <396167189@qq.com>
2021-03-03 18:41:32 +0800

    框架搭建-4-1

--- testCases/test_personal.py
+++ testCases/test_personal.py
@@ -3,8 +3,17 @@
 
 from pageObjects.personal_page import PersonalPage
 
+# log相关导入
+from common.logger import Log
+from pathlib import Path
+
+# logger = Logger().logger(Path(__file__).name) #创建logger的方法从logger()改写到init方法
+log = Log(Path(__file__).name)
+logger = log.get_logger()
+
 class TestPersonal:
     def test_personal_1_goto_personal_tab(self, driver):
+        logger.info("---测试用例---")
         pp = PersonalPage(driver)
         pp.goto_personal_tab()
         # assert pp.get_element()
